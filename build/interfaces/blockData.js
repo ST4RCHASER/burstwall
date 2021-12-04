3390418f90dbc1e0cf1f05f527ddd71e71a285ba
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2021-12-04 23:16:18 +0700

    Delete build directory

--- build/interfaces/blockData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.blockData = void 0;
-class blockData {
-}
-exports.blockData = blockData;
-//# sourceMappingURL=blockData.js.map
